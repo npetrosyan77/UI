6cb7db3b84deff0bd4f3a9f925aec0d5674e8af5
Narine Petrosyan <narinepetrosyan869@gmail.com>
2022-08-04 16:20:42 +0400

    Updaing config file

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,6 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     baseUrl: "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login",
-    projectId: "tooafs"
+    projectId: "qvd6ft"
   },
 });
